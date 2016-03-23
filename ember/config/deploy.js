a28730e8b635a904319901908868f60f70469765
Omar Hamdan <omarhamdan1993@gmail.com>
2016-03-23 13:54:04 +0200

    install ember-cli-deploy-lightning-pack

--- ember/config/deploy.js
+++ ember/config/deploy.js
@@ -1,28 +1,65 @@
-/* jshint node: true */
+var VALID_DEPLOY_TARGETS = [ //update these to match what you call your deployment targets
+  'dev',
+  'qa',
+  'prod'
+];
 
 module.exports = function(deployTarget) {
   var ENV = {
-    build: {}
-    // include other plugin configuration that applies to all deploy targets here
+    build: {},
+    redis: {
+      allowOverwrite: true,
+      keyPrefix: 'emberapp:index'
+    },
+    s3: {
+      prefix: 'emberapp'
+    }
   };
+  if (VALID_DEPLOY_TARGETS.indexOf(deployTarget) === -1) {
+    throw new Error('Invalid deployTarget ' + deployTarget);
+  }
 
-  if (deployTarget === 'development') {
+  if (deployTarget === 'dev') {
     ENV.build.environment = 'development';
-    // configure other plugins for development deploy target here
+    ENV.redis.url = process.env.REDIS_URL || 'redis://0.0.0.0:6379/';
+    ENV.plugins = ['build', 'redis']; // only care about deploying index.html into redis in dev
   }
 
-  if (deployTarget === 'staging') {
+  if (deployTarget === 'qa' || deployTarget === 'prod') {
     ENV.build.environment = 'production';
-    // configure other plugins for staging deploy target here
+    ENV.s3.accessKeyId = process.env.AWS_KEY;
+    ENV.s3.secretAccessKey = process.env.AWS_SECRET;
+    ENV.s3.bucket = /* YOUR S3 BUCKET NAME */;
+    ENV.s3.region = /* YOUR S3 REGION */;
   }
 
-  if (deployTarget === 'production') {
-    ENV.build.environment = 'production';
-    // configure other plugins for production deploy target here
+  if (deployTarget === 'qa') {
+    ENV.redis.url = process.env.QA_REDIS_URL;
   }
 
-  // Note: if you need to build some configuration asynchronously, you can return
-  // a promise that resolves with the ENV object instead of returning the
-  // ENV object synchronously.
+  if (deployTarget === 'prod') {
+    ENV.redis.url = process.env.PROD_REDIS_URL;
+  }
+
   return ENV;
-};
+
+  /* Note: a synchronous return is show above, but ember-cli-deploy
+   * does support returning a promise, in case you need to get any of
+   * your configuration asynchronously. e.g.
+   *
+   *    var Promise = require('ember-cli/lib/ext/promise');
+   *    return new Promise(function(resolve, reject){
+   *      var exec = require('child_process').exec;
+   *      var command = 'heroku config:get REDISTOGO_URL --app my-app-' + deployTarget;
+   *      exec(command, function (error, stdout, stderr) {
+   *        ENV.redis.url = stdout.replace(/\n/, '').replace(/\/\/redistogo:/, '//:');
+   *        if (error) {
+   *          reject(error);
+   *        } else {
+   *          resolve(ENV);
+   *        }
+   *      });
+   *    });
+   *
+   */
+}
